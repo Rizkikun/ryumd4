317b09bb385c579e214caf8a01d8f01ea0d0a7f2
Rizkikun <101055276+Rizkikun@users.noreply.github.com>
2022-03-27 06:08:39 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -20,9 +20,9 @@ global.author = 'ikybotz By Rizki' //Sticker Weem
 global.webme = 'https://api-ryubotz.herokuapp.com'
 global.sessionName = 'session'
 global.bg_welcome = 'https://telegra.ph/file/528b594d5d91c08482515.jpg' //Max 30kb Agar Tidak Delay
-global.youtube = 'https://youtube.com/c/RyuukaBotz'
-global.github = 'https://github.com/R-TEAM94'
-global.instagram = 'https://instagram.com/fandyyy._'
+global.youtube = 'https://youtube.com/channel/UC4nKoTls09OfD-GUTKt-6OA'
+global.github = 'https://github.com/rizkiwibu'
+global.instagram = '-._'
 
 //===> BACKGROUND SETIAP MENU
 global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
